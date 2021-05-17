9bd996bc1ff7f052978ecd44646f52ff9a5eea3a
Kanishkavikram Purohit <sci.29.9.99@gmail.com>
2021-05-17 17:39:21 +0530

    Added more basic functionality

--- lib/projconst/projconst.h
+++ lib/projconst/projconst.h
@@ -3,15 +3,23 @@
  */
 
 // Internal communication Device IDs
-#define MYID 1                                             // ID of this device. Ranges bet'n  1 to 254
-#define VSENSE_NUM 3                                       // Number of slaves for voltage sensing
-#define VSENSE_MIN 2                                       // ** DO NOT CHANGE THIS VALUE **
-#define FCTRL_NUM 2                                        // Number of slaves for fan control
-#define FCTRL_MIN 129                                      // ** DO NOT CHANGE THIS VALUE **
-#define ISENSE_SLAVE 127                                   // Slave ID of current sensing slave
-#define CELL_MAX 90                                        // Number of cells in the system
-#define VSENSE_MAX_CELLS 15                                // Maximum number of cells per slave
+#define MYID                1                                       // ID of this device. Ranges bet'n  1 to 254
+#define VSENSE_NUM          3                                       // Number of slaves for voltage sensing. Must be betn 1 and 126
+#define VSENSE_MIN          2                                       // ** DO NOT CHANGE THIS VALUE **
+//#define FCTRL_NUM           2                                       // Number of slaves for fan control
+//#define FCTRL_MIN           129                                     // ** DO NOT CHANGE THIS VALUE **
+#define ISENSE_SLAVE        128                                     // Slave ID of current sensing slave
+#define CELL_MAX            90                                      // Number of cells in the system
+#define VSENSE_MAX_CELLS    15                                      // Maximum number of cells per slave
+#define TSENSE_MAX          5                                       // Maximum number of temperature sensors per 
+#define TEMP_TARGET         55                                      // Temperature target for the temperature sensors
+#define TEMP_HYST_EN        5
+#define TEMP_HYST_DIS       10
 
 // Internal communication Message IDs
-#define PING 2
-#define VAL_PREFIX 1
+#define PING                1
+#define VOLT_REQ            2
+#define ISENSE_REQ          3
+#define TEMP_REQ            4
+#define FAN_CMD_EN          5
+#define FAN_CMD_DIS         5
--- src/main.cpp
+++ src/main.cpp
@@ -54,18 +54,29 @@ int slave_ping(int);
 void slave_tx(int, int, int);
 String slave_rx(void);
 float read_current(void);
-float read_voltage(int);
+float read_voltage(uint8_t,uint8_t);
+float read_temp(uint8_t,uint8_t);
+void fan_ctrl(uint8_t,uint8_t,bool);
 
 /* 
  * Global variables
  */
 // Error handling
-int err_flag=0;
+int8_t err_flag=0;
 // CAN
 unsigned char CAN_Recv_Flag = 0;
 unsigned char len = 0;
 unsigned char buf[8];
 char str[20];
+
+// 1 cycle old data
+float sysCurrent_old = 0;
+float cellVoltages_old[CELL_MAX];
+float cellTemps_old[TSENSE_MAX*VSENSE_NUM];
+// Current cycle data
+float sysCurrent = 0;
+float cellVoltages[CELL_MAX];
+float cellTemps[TSENSE_MAX*VSENSE_NUM];
 
 void setup() {
   // put your setup code here, to run once:
@@ -110,7 +121,7 @@ void setup() {
   }
 
   // Fan Control Slaves
-  for(int i=FCTRL_MIN; i<=(FCTRL_MIN+FCTRL_NUM); i++) {
+  /*for(int i=FCTRL_MIN; i<=(FCTRL_MIN+FCTRL_NUM); i++) {
     Serial.print("Fan Control Slave " + i);
     if(slave_ping(i)){
       Serial.println(" is alive.");
@@ -118,7 +129,7 @@ void setup() {
       Serial.println("is not responding. Please check the system.");
       err_flag = ENOSLAVE_NF;
     }
-  }
+  }*/ // TO be done through vsense and temp sense slaves
 
 }
 
@@ -137,18 +148,43 @@ void loop() {
   Serial.println("Received CAN data. Parsing not implemented yet."); 
 
   // Acquire current flow
-  float sysCurrent = read_current();
+  sysCurrent = read_current();
 
   // Acquire cell voltages
-  int cellVoltages[CELL_MAX];
-  for(int i=0; i < CELL_MAX; i++){
-    // Get cell stuff
-    cellVoltages[i] = read_voltage(i);
+  for(int iSlaveNumber=0; iSlaveNumber < (CELL_MAX/VSENSE_MAX_CELLS); iSlaveNumber++){
+    for(int jCellNumber=0; jCellNumber < VSENSE_MAX_CELLS; jCellNumber++){
+      // Get cell stuff
+      //cellVoltages[i] = read_voltage(i);
+      cellVoltages[iSlaveNumber*jCellNumber] = read_voltage(iSlaveNumber,jCellNumber);
+    }
   }
 
   // Acquire cell temperatures
+  for(int iSlaveNumber=0; iSlaveNumber < (CELL_MAX/VSENSE_MAX_CELLS); iSlaveNumber++){
+    for(int jCellNumber=0; jCellNumber < TSENSE_MAX; jCellNumber++){
+      // Get cell stuff
+      cellTemps[iSlaveNumber*jCellNumber] = read_temp(iSlaveNumber,jCellNumber);
+    }
+  }
 
   // Adjust fan states if needed
+  for (int fanNumber = 0; fanNumber < VSENSE_NUM; fanNumber++) {
+    for (int sensorNumber; sensorNumber < TSENSE_MAX; sensorNumber++) {
+      if (cellTemps[fanNumber*sensorNumber] < TEMP_TARGET-TEMP_HYST_DIS) {
+        fan_ctrl(fanNumber, sensorNumber,0);
+      } else if (cellTemps[fanNumber*sensorNumber] > TEMP_TARGET-TEMP_HYST_EN) {
+        fan_ctrl(fanNumber, sensorNumber,1);
+      }
+    }
+  }
+
+  sysCurrent_old = sysCurrent;
+  for (int i = 0; i < (sizeof(cellVoltages)/sizeof(float)); i++) {
+    cellVoltages_old[i] = cellVoltages[i];
+  }
+  for (int i = 0; i < (sizeof(cellTemps)/sizeof(float)); i++) {
+    cellTemps_old[i] = cellTemps[i];
+  }
 
 }
 
@@ -163,10 +199,12 @@ int slave_ping(int destID)
   //Ping the slave here
   slave_tx(destID, 0, 0);
   // Implement Rx function, check response, pass if ping passed.
-  String ping_response = slave_rx();
-  CSV_Parser cp(ping_response.c_str(),"-ud--",false);
+  String pingResponse = slave_rx();
+  CSV_Parser cp(pingResponse.c_str(),"i--i",false);
   cp.parseLeftover();
-  if ((int16_t)cp[0] == destID && (int16_t)cp[3] == destID) {
+  int16_t *tempID1 = (int16_t*)cp[0];
+  int16_t *tempID2 = (int16_t*)cp[0]; 
+  if (tempID1[0] == destID && tempID2[0] == destID) {
     ret = 0;
   }
   return ret;
@@ -221,15 +259,55 @@ float read_current(void)
   float resp = 5000;
   slave_tx(ISENSE_SLAVE,10,0);
   // Implement Rx function, return response
-  /*
-  resp = slave_rx_float(ISENSE_SLAVE);
-   */
+  String isenseResponse = slave_rx();
+  CSV_Parser cp(isenseResponse.c_str(),"i--f",false);
+  cp.parseLeftover();
+  int8_t *tempIsenseID = (int8_t*)cp[0];
+  if (tempIsenseID[0] == ISENSE_SLAVE) {
+    float *tempIsenseBuffer = (float*)cp[3];
+    resp = tempIsenseBuffer[0];
+  }
   return resp;
 }
 
-float read_voltage(int)
+float read_voltage(uint8_t slaveID,uint8_t cellID)
 {
   float ret=5.5;
-  //slave_tx();
+  slave_tx(slaveID,VOLT_REQ,cellID);
+  String vSenseResponse = slave_rx();
+  CSV_Parser cp(vSenseResponse.c_str(),"i-if",false);
+  cp.parseLeftover();
+  int8_t *vSenseSlaveID = (int8_t*)cp[0];
+  int8_t *vSenseMessageID = (int8_t*)cp[2];
+  if (vSenseSlaveID[0] == slaveID) {
+    if (vSenseMessageID[0] == cellID) {
+      float *tempvSenseResponse = (float*)cp[3];
+      ret = tempvSenseResponse[0];
+    }
+  }
   return ret;
+}
+
+float read_temp(uint8_t slaveID,uint8_t tempID)
+{
+  float ret=99.99;
+  slave_tx(slaveID,TEMP_REQ,tempID);
+  String vSenseResponse = slave_rx();
+  CSV_Parser cp(vSenseResponse.c_str(),"i-if",false);
+  cp.parseLeftover();
+  int8_t *vSenseSlaveID = (int8_t*)cp[0];
+  int8_t *vSenseMessageID = (int8_t*)cp[2];
+  if (vSenseSlaveID[0] == slaveID) {
+    if (vSenseMessageID[0] == tempID) {
+      float *tempvSenseResponse = (float*)cp[3];
+      ret = tempvSenseResponse[0];
+    }
+  }
+  return ret;
+}
+
+void fan_ctrl(uint8_t slaveID,uint8_t fanNum,bool status) 
+{
+  // stuff
+  slave_tx(slaveID,(status?FAN_CMD_EN:FAN_CMD_DIS),fanNum);
 }
